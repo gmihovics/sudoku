8619e85bf53f990eb5fbabbf146fcf952e4254d9
Garrett Mihovics <gmihovic@opentext.com>
2018-04-30 08:42:12 -0400

    Add react components to render a sudoku puzzle

--- src/frontend/components/Puzzle.jsx
+++ src/frontend/components/Puzzle.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class Puzzle extends Component {
+  static renderSquare(index, number) {
+    return <Square key={index} number={number} />;
+  }
+
+  constructor() {
+    super();
+
+    this.state = {
+      cells: new Array(81).fill(0)
+    };
+  }
+
   render() {
+    const board = [];
+
+    for (let i = 0; i < 9; i += 1) {
+      const row = [];
+
+      for (let j = 0; j < 9; j += 1) {
+        row.push(Puzzle.renderSquare((i * 9) + j, this.state.cells[(i * 9) + j]));
+      }
+
+      board.push(<div className="board-row" key={i}>{row}</div>);
+    }
+
     return (
-      <div />
-    );
+      <div>
+        {board}
+      </div>
+    )
   }
 }
 
